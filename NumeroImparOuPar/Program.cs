67696a95cf6cc09348edd28f427f61941c3ce6ca
nicolasbalduino <nicolasbalduino@outlook.com>
2023-03-04 18:42:26 -0300

    criada funcao que informa se o numero e par ou impar

--- NumeroImparOuPar/Program.cs
+++ NumeroImparOuPar/Program.cs
@@ -1,9 +1,14 @@
 ﻿int n1;
 
+void ParImpar()
+{
+    if (n1 % 2 == 0)
+        Console.WriteLine("O numero é par!");
+    else
+        Console.WriteLine("O numero é impar!");
+}
+
 Console.WriteLine("Informe um numero: ");
 n1 = int.Parse(Console.ReadLine());
 
-if (n1 % 2 == 0)
-    Console.WriteLine("O numero é par!");
-else
-    Console.WriteLine("O numero é impar!");
+ParImpar();
