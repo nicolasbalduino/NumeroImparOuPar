5c124c9d4ff8de6c150fcdb273d74faba57bae51
nicolasbalduino <nicolasbalduino@outlook.com>
2023-03-04 17:49:03 -0300

    Add project files.

---
+++ NumeroImparOuPar/Program.cs
@@ -0,0 +1,9 @@
+﻿int n1;
+
+Console.WriteLine("Informe um numero: ");
+n1 = int.Parse(Console.ReadLine());
+
+if (n1 % 2 == 0)
+    Console.WriteLine("O numero é par!");
+else
+    Console.WriteLine("O numero é impar!");
